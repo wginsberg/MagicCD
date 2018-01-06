01b64b7368d24a75bb91962070c88fbf32a64792
Will Ginsberg <will.j.ginsberg@gmail.com>
2018-01-06 00:28:45 -0500

    Stop being a fancy pants and make the code much more simple

--- library.py
+++ library.py
@@ -4,106 +4,50 @@ import os
 import argparse
 
 DEFAULT_LIBRARY_PATH = os.path.join(os.environ["HOME"], ".rfid_library.csv")
-DEFAULT_DELIMETER = ","
 
-class Library:
-	"""
-	Persistent (music) library
-	"""
-
-	def __init__(self, path, mode="r", delimeter=None):
-		"""
-		Any mode other than "w" and "w+" should work ok
-		"""
-
-		self.path = DEFAULT_LIRBRARY_PATH if path is None else path
-		
-		self.mode = mode
-		self.delimeter = DEFAULT_DELIMETER if delimeter is None else delimeter
-
-		self.file = None
-		self.entries = {}
-
-	def __enter__(self):
-		self.file = open(self.path, self.mode)
-
-		if self.readable():
-			lines = self.file.read().split("\n")
-			for line in lines:
-				if not len(line):
-					continue
-				key, value = line.split(self.delimeter)
-				self.entries[key] = value
-		
-		if not self.writeable():
-			self.file.close()
-
-		return self
-
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		if self.writeable():
-			self.file.close()
-
-	def readable(self):
-		return self.mode == "r" or "+" in self.mode
-
-	def writeable(self):
-		return self.mode != "r"
-
-	def __getitem__(self, key):
-		if not self.readable():
-			raise IOError("Can't read from write-only Library instance")
-		return self.entries[key]
-
-	def __contains__(self, key):
-		if not self.readable():
-			raise IOError("Can't read from write-only Library instance")
-		return key in self.entries
-
-	def __setitem__(self, key, value):
-		if not self.writeable():
-			raise IOError("Can't write to read-only Library instance")
-	
-		if key in self.entries:
-			raise IOError("Key already exists")
-	
-		self.file.write("{},{}\n".format(key, value))
-		self.entries[key] = value
-
-
-def add_cwd_to_library(path, delimeter=None):
+def add_cwd_to_library(path):
 
 	identifier = raw_input("Please input the identifier for this library entry\n")
 	entry = os.getcwd()
 
-	with Library(path, "a", delimeter) as library:
-		library[identifier] = entry
+	with open(path, "a") as f:
+		f.write("{},{}".format(identifier, entry))
 
 	print "Success: {} -> {}".format(identifier, entry)
 
 
-def lookup(path, delimeter=None):
-
-	identifier = raw_input("Please input the identifier\n")
+def lookup(path):
 	
-	with Library(path, "r", delimeter) as library:
-		entry = None
-		if identifier in library:
-			entry = library[identifier]
+	with open(path, "r") as f:
 
-	if entry is None:
-		print "Entry {} is not in the library".format(identifier)
-	else:
-		print "{} -> {}".format(identifier, entry)
+		library = {}		
+		for line in f.read().split("\n"):
+			if not line:
+				continue
+			identifier = line.split(",")[0]
+			entry = line.split(",")[1]
+			library[identifier] = entry
+		
+		while True:
 
-def init(path, delimeter=None):
+			identifier = raw_input("Please input an identifier (Empty line to exit)\n")
+			if not len(identifier):
+				break
+			
+			entry = library.get(identifier, None)
+			if entry is None:
+				print "Entry {} is not in the library".format(identifier)
+			else:
+				print "{} -> {}".format(identifier, entry)
 
+
+def init(path):
 
 	if os.path.exists(path):
-		confirmation = raw_input("Library already exists. Delete? [y/n]\n")
+		confirmation = raw_input("Library already exists. Overwrite? [y/n]\n")
 		if confirmation != "y":
 			print "Aborting."
-		os.path.remove(path)
+		os.remove(path)
 
 	open(path, "w").close()
 	print "Library initialized at {}".format(path)
@@ -135,7 +79,8 @@ if __name__ == "__main__":
 			   action="store_const",
 			   dest="action",
 			   const=lookup,
-			   help="Look up an entry")
+			   help="Look up entries in the library")
 	args = parser.parse_args()
 
 	args.action(args.f)
+
